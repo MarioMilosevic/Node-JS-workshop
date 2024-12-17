7b6b06585064c85270d3bab292fd05e005b136f4
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-12-17 19:00:47 +0100

    EventEmmiter exercise

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello world Mario");
+import EventEmitter from "events";
+
+const emitter = new EventEmitter();
+
+emitter.on("order-pizza", (size, topping) => {
+  console.log(`Order received! Baking a pizza a ${size} pizza with ${topping}`);
+});
+
+emitter.on("order-pizza", (size) => {
+  if (size === "large") {
+    console.log("Serving complimentary drink");
+  }
+});
+
+console.log("Before emit");
+emitter.emit("order-pizza", "large", "mushrooms");
+console.log("After emit");
