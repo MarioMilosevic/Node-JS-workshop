f66de39f713835352bd7fd46352712c201fa08f3
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-12-19 13:16:42 +0100

    Exercuse

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-import { createServer } from "node:http";
+setTimeout(() => {
+  console.log("this is setTimeout 1");
+}, 1000);
+setTimeout(() => {
+  console.log("this is setTimeout 2");
+}, 500);
+setTimeout(() => {
+  console.log("this is setTimeout 3");
+}, 0); 
+// import crypto from "node:crypto";
 
-const server = createServer((req, res) => {
-  const superHero = {
-    fistName: "Bruce",
-    lastName: "Wayne",
-  };
+// const start = Date.now();
+// crypto.pbkdf2Sync("password", "salt", 10000, 512, "sha512");
+// console.log("Hash", Date.now() - start);
 
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(superHero));
-  console.log(req);
-});
+// // import { createServer } from "node:http";
+// // import fs from "node:fs";
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+// // const server = createServer((req, res) => {
+// //   res.writeHead(200, { "Content-Type": "text/html" });
+// //   fs.createReadStream("./index.html").pipe(res)
+// //   // const html = fs.readFileSync("./index.html", "utf-8");
+// //   // res.end(html);
+// // });
+
+// // server.listen(3000, () => {
+// //   console.log("Server running on port 3000");
+// // });
