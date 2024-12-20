6b3d03a2f381afdd6de34d33b499448753becb60
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-12-20 14:36:17 +0100

    Remove extra files

--- math.js
+++
@@ -1,9 +0,0 @@
-const add = (a, b) => {
-    return a + b
-}
-
-const subrtact = (a, b) => {
-    return a - b
-}
-
-export default {add, subrtact}
--- superman.js
+++
@@ -1,3 +0,0 @@
-const superhero = "Superman";
-console.log(superhero);
-
