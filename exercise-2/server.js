aa86048f8922bcd3a355fcca6ae020931a364204
Mario Milošević <mario.milosevic@rbt.rs>
2026-02-23 07:54:38 +0100

    second exercise

---
+++ exercise-2/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/about.html");
+});
+app.get("/contact-me", (req, res) => {
+  res.sendFile(__dirname + "/contact-me.html");
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(__dirname + "/404.html");
+});
+
+app.listen(8080, () => {
+  console.log("Server listening on port 8080");
+});
