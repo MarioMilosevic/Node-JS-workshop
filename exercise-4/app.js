71a72264f178cfa5a0753218a397ce4bfdeebc3f
Mario Milošević <mario.milosevic@rbt.rs>
2026-02-26 22:38:50 +0100

    template engine

---
+++ exercise-4/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("node:path");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
+app.get("/", (req, res) => {
+  res.render("index", { links: links, users: users });
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
